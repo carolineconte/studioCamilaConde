3f3efda9514ed6a899e024917dc1a0c1242aa148
Caroline Conte <carolinecontee@gmail.com>
2024-05-05 15:07:36 +0200

    erro

--- src/components/layout/Gallery.tsx.tsx
+++ src/components/layout/Gallery.tsx.tsx
@@ -6,7 +6,7 @@ interface Props {
   img3: string;
   img4: string;
   img5: string;
-  img6: string;
+  img6?: string;
   text1?: string;
   text2?: string;
   text3?: string;
@@ -14,7 +14,7 @@ interface Props {
   text5?: string;
 }
 
-export default function Gallery({ img1, text1, img2, text2, img3, text3, img4, text4, img5, img6, text5 } : Props) {
+export default function Gallery({ img1, text1, img2, text2, img3, text3, img4, text4, img5, img6, text5 }: Props) {
   return (
 
     <div className="py-4 px-2 mx-auto max-w-screen-xl sm:py-4 lg:px-24">
@@ -30,8 +30,8 @@ export default function Gallery({ img1, text1, img2, text2, img3, text3, img4, t
         </div>
 
         <div className="col-span-2 sm:col-span-1 md:col-span-2 bg-stone-50">
-       
-        <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 lg:grid-cols-2">
+
+          <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 lg:grid-cols-2">
             <div className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
               <Image loading="lazy" width={200} height={200} src={img2} alt="" className="absolute inset-0 h-full w-full object-cover object-top group-hover:scale-105 transition-transform duration-500 ease-in-out" />
               <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
@@ -57,14 +57,18 @@ export default function Gallery({ img1, text1, img2, text2, img3, text3, img4, t
             </div>
           </div>
         </div>
+        {
+          img6 && (
+            <div className="col-span-2 sm:col-span-1 md:col-span-1 bg-sky-50 h-auto md:h-full flex flex-col">
+              <div className="group relative hidden md:flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow">
+                <Image loading="lazy" width={200} height={200} src={img6} alt="" className="absolute inset-0 h-full w-full object-cover  object-top  group-hover:scale-105 transition-transform duration-500 ease-in-out" />
+                <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
+                <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">{text5}</h3>
+              </div>
+            </div>
+          )
+        }
 
-        <div className="col-span-2 sm:col-span-1 md:col-span-1 bg-sky-50 h-auto md:h-full flex flex-col">
-          <div className="group relative hidden md:flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow">
-            <Image loading="lazy" width={200} height={200} src={img6} alt="" className="absolute inset-0 h-full w-full object-cover  object-top  group-hover:scale-105 transition-transform duration-500 ease-in-out" />
-            <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
-            <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">{text5}</h3>
-          </div>
-        </div>
 
       </div>
     </div>
